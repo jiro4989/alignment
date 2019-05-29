c3b80fc88142c16e8134c1b43a2247130c95bb3a
jiro4989 <jiroron666@gmail.com>
2019-05-29 16:58:49 +0900

    :construction: WIP

---
+++ src/align.nim
@@ -0,0 +1,10 @@
+import eastasianwidth
+
+proc alignLeft*(lines: openArray[string], pad = " "): seq[string] =
+  discard
+
+proc alignCenter*(lines: openArray[string], pad = " "): seq[string] =
+  discard
+
+proc alignRight*(lines: openArray[string], pad = " "): seq[string] =
+  discard
