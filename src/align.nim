72de548c6e02779b1b66fd8afdfbb97ef9ae566e
jiro4989 <jiroron666@gmail.com>
2019-05-29 18:06:18 +0900

    :recycle: refactor

--- src/align.nim
+++ src/align.nim
@@ -5,14 +5,14 @@ from sequtils import mapIt
 proc alignLeft*(lines: openArray[string], pad = " "): seq[string] =
   let lineMaxWidth = lines.mapIt(it.stringWidth).max
   for line in lines:
-    let diff = lineMaxWidth - line.stringWidth
+    let
+      diff = lineMaxWidth - line.stringWidth
+      repeatCount = int(diff / pad.stringWidth)
     var s = line
-    if 0 < diff:
-      let repeatCount = int(diff / pad.stringWidth)
-      if 0 < repeatCount:
-        let pads = pad.repeat(repeatCount).join
-        s.add " ".repeat(lineMaxWidth - line.stringWidth - pads.stringWidth).join
-        s.add pads
+    if 0 < repeatCount:
+      let pads = pad.repeat(repeatCount).join
+      s.add " ".repeat(lineMaxWidth - line.stringWidth - pads.stringWidth).join
+      s.add pads
     result.add s
 
 proc alignCenter*(lines: openArray[string], pad = " ", marginLeft = 0, marginRight = 0): seq[string] =
